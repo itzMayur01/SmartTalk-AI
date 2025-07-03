6ae732a7b3be16290e6991651b47fb8b0f9bc8f8
Mayur kumar <109099752+itzMayur01@users.noreply.github.com>
2025-07-03 22:07:19 +0530

    Update Register.jsx
    copy right

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -157,7 +157,7 @@ const Register = () => {
             </p>
           </div>
           <p className='mt-auto mx-auto text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant text-bodyMedium lg:mx-0'>
-            &copy; 2024 Mayur. All right reserved
+            &copy; 2025 Thakur Sahab. All right reserved
           </p>
         </div>
         <div className='hidden img-box lg:block lg:relative lg:rounded-large lg:overflow-hidden'>
